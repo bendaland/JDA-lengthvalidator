0518df7a4793f819a352623a72f5cfedbfc00c76
bendaland <bendaland@users.noreply.github.com>
2014-10-17 14:32:12 -0500

    Update plugin.js

--- js/plugin.js
+++ js/plugin.js
@@ -3,25 +3,25 @@
         options = $.extend({}, $.fn.LengthValidator.defaults, options);
 
         return this.each(function () {
-			var elm = $(this);
+		var elm = $(this);
 			
-			checkLength($(this), options, $.trim(elm.val()).length);
+		checkLength($(this), options, $.trim(elm.val()).length);
 			
-			elm.focusin(function () { checkLength($(this), options, $.trim(elm.val()).length); })
-				.focusout(function () { checkLength($(this), options, $.trim(elm.val()).length); })
-				.change(function () { checkLength($(this), options, $.trim(elm.val()).length); });		 
+		elm.focusin(function () { checkLength($(this), options, $.trim(elm.val()).length); })
+			.focusout(function () { checkLength($(this), options, $.trim(elm.val()).length); })
+			.change(function () { checkLength($(this), options, $.trim(elm.val()).length); });		 
         });
     }; 
 
     function checkLength(elm, opts, len) {
-		if(elm.hasClass(opts.valid)){ elm.removeClass(opts.valid); }
-		if(elm.hasClass(opts.invalid)){ elm.removeClass(opts.invalid); }
+	if(elm.hasClass(opts.valid)){ elm.removeClass(opts.valid); }
+	if(elm.hasClass(opts.invalid)){ elm.removeClass(opts.invalid); }
 	
-		if( len < opts.min || len > opts.max ){
-			elm.addClass(opts.invalid);
-		} else {
-			elm.addClass(opts.valid);
-		} 
+	if( len < opts.min || len > opts.max ){
+		elm.addClass(opts.invalid);
+	} else {
+		elm.addClass(opts.valid);
+	} 
     }
 
     $.fn.LengthValidator.defaults = {
